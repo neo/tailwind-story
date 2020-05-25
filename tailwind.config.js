857d7e2bb9897a675c995d2cf57b56253a4c2004
Wenchen Li <neolwc@gmail.com>
2020-05-24 20:00:59 -0400

    Initial draft

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   theme: {
-    extend: {},
+    extend: {
+      transitionTimingFunction: {
+        "out-expo": "cubic-bezier(0.19, 1, 0.22, 1)",
+      },
+    },
   },
   variants: {},
   plugins: [],
